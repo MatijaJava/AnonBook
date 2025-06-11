eb229c72bae2e371f0a05e51c236065cc1261ed8
marko <orovic.matija@gmail.com>
2025-06-11 17:28:48 +0200

    1. commit

---
+++ src/main/java/com/matija/anonbook/AnonBookApplication.java
@@ -0,0 +1,13 @@
+package com.matija.anonbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnonBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/matija/anonbook/AnonBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matija.anonbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
