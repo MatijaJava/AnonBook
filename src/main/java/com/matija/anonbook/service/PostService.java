66464c0484d6ad50290a47aa94af566019aaa7ec
marko <orovic.matija@gmail.com>
2025-06-11 19:22:47 +0200

    vrati 1 post vraca sve komentare

--- src/main/java/com/matija/anonbook/service/PostService.java
+++ src/main/java/com/matija/anonbook/service/PostService.java
@@ -1,6 +1,8 @@
 package com.matija.anonbook.service;
 
+import com.matija.anonbook.model.Komentar;
 import com.matija.anonbook.model.Post;
+import com.matija.anonbook.repository.KomentarRepository;
 import com.matija.anonbook.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,8 @@ public class PostService {
 
     @Autowired
     private PostRepository postRepository;
+    @Autowired
+    private KomentarRepository komentarRepository;
 
     public List<Post> vratiPostove() {
         return postRepository.findAll();
@@ -21,7 +25,12 @@ public class PostService {
     public Post vratiJedanPost(Integer postId) {
         Optional<Post> optionalPost = postRepository.findById(postId);
         if (optionalPost.isPresent()){
-            return optionalPost.get();
+            Post post = optionalPost.get();
+            List<Komentar> komentari = komentarRepository.findAllByPost(post);
+
+            post.setKomentari(komentari);
+
+            return post;
         } else {
             return null;
         }
