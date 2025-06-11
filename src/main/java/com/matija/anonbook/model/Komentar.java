d64e00a37b17fda9db1114e60338d3fe72b83f84
marko <orovic.matija@gmail.com>
2025-06-11 17:47:00 +0200

    anotirane domenske klase

--- src/main/java/com/matija/anonbook/model/Komentar.java
+++ src/main/java/com/matija/anonbook/model/Komentar.java
@@ -1,17 +1,25 @@
 package com.matija.anonbook.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Komentar {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
     private Post post;
+
     private String text;
     private String potpis;
 
--- src/main/java/com/matija/anonbook/model/Post.java
+++ src/main/java/com/matija/anonbook/model/Post.java
@@ -1,5 +1,6 @@
 package com.matija.anonbook.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,14 +8,22 @@ import lombok.Setter;
 
 import java.util.List;
 
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Post {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
     private String text;
+
     private String potpis;
+
+    @OneToMany
     private List<Komentar> komentari;
 
 }
