8a6e5682b1c7e0dae460c9f26525c1ab4820da80
marko <orovic.matija@gmail.com>
2025-06-11 17:37:06 +0200

    popunjene domenske klase

--- src/main/java/com/matija/anonbook/model/Komentar.java
+++ src/main/java/com/matija/anonbook/model/Komentar.java
@@ -1,4 +1,18 @@
 package com.matija.anonbook.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Komentar {
+
+    private Post post;
+    private String text;
+    private String potpis;
+
 }
--- src/main/java/com/matija/anonbook/model/Post.java
+++ src/main/java/com/matija/anonbook/model/Post.java
@@ -1,4 +1,20 @@
 package com.matija.anonbook.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Post {
+
+    private String text;
+    private String potpis;
+    private List<Komentar> komentari;
+
 }
