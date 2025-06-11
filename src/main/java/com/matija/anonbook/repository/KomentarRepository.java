5b9e357455f3dd3e3818782f981bbeeb3ab0302d
marko <orovic.matija@gmail.com>
2025-06-11 19:22:27 +0200

    findAllByPost

--- src/main/java/com/matija/anonbook/repository/KomentarRepository.java
+++ src/main/java/com/matija/anonbook/repository/KomentarRepository.java
@@ -1,9 +1,13 @@
 package com.matija.anonbook.repository;
 
 import com.matija.anonbook.model.Komentar;
+import com.matija.anonbook.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface KomentarRepository extends JpaRepository<Komentar, Integer> {
+    List<Komentar> findAllByPost(Post post);
 }
