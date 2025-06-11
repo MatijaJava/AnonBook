bc2c6a5ef4aec9e2d2484f0d99a49f740bfe0905
marko <orovic.matija@gmail.com>
2025-06-11 19:11:00 +0200

    get 1 post

--- src/main/java/com/matija/anonbook/rest/PostRest.java
+++ src/main/java/com/matija/anonbook/rest/PostRest.java
@@ -4,6 +4,7 @@ import com.matija.anonbook.model.Post;
 import com.matija.anonbook.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +28,10 @@ public class PostRest {
         return postService.vratiPostove();
     }
 
+    @GetMapping("/{postId}")
+    public Post vratiJedanPost(@PathVariable Integer postId){
+        return postService.vratiJedanPost(postId);
+    }
 
 
 }
--- src/main/java/com/matija/anonbook/service/PostService.java
+++ src/main/java/com/matija/anonbook/service/PostService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostService {
@@ -15,5 +16,14 @@ public class PostService {
 
     public List<Post> vratiPostove() {
         return postRepository.findAll();
+    }
+
+    public Post vratiJedanPost(Integer postId) {
+        Optional<Post> optionalPost = postRepository.findById(postId);
+        if (optionalPost.isPresent()){
+            return optionalPost.get();
+        } else {
+            return null;
+        }
     }
 }
